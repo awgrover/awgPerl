c3832439d52c0204197ae9df2a1c90edad3a7749
Alan Grover <awgrover@msen.com>
2010-10-10 13:39:39 -0400

    cutnpaste_template.pm: remove the \ on escaped things; more comments

--- cutnpaste_template.pm
+++ cutnpaste_template.pm
@@ -18,30 +18,52 @@ sub render {
     # $template = "rest" for next split
     # Split gives just "head" if no "@", so processes each piece
     # avoid $' with a split
-    while (my @repeats = split(/(?(?<=[^\\])|^)@([a-zA-Z]\w*|_)(?=\[)/, $template,2)) {
-        # interpolates the scalars in the "head" piece
-        $repeats[0] =~ s/(?(?<=[^\\])|^)\$([a-zA-Z]\w*|_|[0-9])/(index('0123456789',$1) >= 0) ? $data->{'_'}->[$1] : $data->{$1}/eg;
-        push @rez, $repeats[0];
-        last if ! $repeats[1];
+    # We actually split on \@ or @, so we can remove the \
+    while (my ($head, $escape, $field_name, $block) = split(/(\\?)@([a-zA-Z]\w*|_)(?=\[)/, $template,2)) {
 
-        my $field_name = $repeats[1];
-        # warn "During '\@$field_name'";
+        # this was a \@word[..., so remove the \
+        if ($escape) {
+          $head = $head."@".$field_name;
+          }
+
+        # fix bracket escapes
+        $head =~ s/\\\[/[/g;
+        $head =~ s/\\\]/]/g;
+
+        # interpolate scalars in the "head"
+        # Have to capture the \ so we can do the right thing
+        my $interp = sub {
+          # escape, fieldname
+          if ($1) { '$'.$2 } # escaped, so remove /
+          elsif (index('0123456789',$2) >= 0) { 
+            $data->{'_'}->[$2]; 
+            }
+          else { $data->{$2}; } # interp
+          };
+        $head =~ s/(\\?)\$([a-zA-Z]\w*|_|[0-9])/&$interp/eg;
+        push @rez, $head;
+
+        last if ! $field_name;
+
+        # this was a \@word[..., so next on ...
+        if ($field_name =~ /^\\(.+)/) {
+          $template = $block;
+          next;
+          }
+          
 
         # Get the "block" and "rest"
         my $bracketed;
-        ($bracketed, $template) = extract_bracketed( $repeats[2], '[');
+        ($bracketed, $template) = extract_bracketed( $block, '[');
         $bracketed =~ s/^\[//;
         $bracketed =~ s/\]$//;
 
         # Repeat the block
         my $list = (ref($data->{$field_name}) eq 'ARRAY') ? $data->{$field_name} : [$data->{$field_name}];
-        # warn "To repeat '\@$field_name', ".@$list." times";
         foreach my $sub_data ( @$list ) {
             # recurse on this block with our block's data
-            # warn "\tWith $sub_data ",(ref($sub_data) eq 'ARRAY' && @$sub_data);
             push @rez, render( $bracketed, {%$data, (ref($sub_data) eq 'HASH' ? %$sub_data : ()), '_' => $sub_data});
             }
-        # warn "Finished:'\@$field_name'";
         }
 
     join("",@rez);
@@ -89,17 +111,17 @@ main() if $0 eq __FILE__;
 
 __DATA__
 Test Template
-Interpolates only "names" following @ and $, these don't work: @123 @123dog @["dog" xx] $"dog"
+Interpolates only "names" following @ and $, these don't work: @123 @123dog @["dog"] $"dog"
 Iterates a block: \@something[ stuff repeated ]
 Within an iteration, also iterpolates \$_ and \$0..\$9 (see below)
 Must otherwise balance left and right [] in iterations so the \@xxx[...] works
 
 Literal @ and literal $ (because no "word" after sigil)
 Escape dollar-sign: \$name
-Escape at-sign \@outer[]
+Escape at-sign \@outer[] \@outer[ x ]
 Escape un-balanced brackets \] \[ (especially inside iterations)
 Catenate interpolation: pretend scalar is array: Prefix@name[$_]Suffix
-Can't escape backslash \\@outer[]
+Can't escape backslash \\, but \\@outer[] is \ followed by \@..., thus dropping one \
 No re-interpolation: $has_dollar_in_value
 
 Top level scalar interpolation of \$file: $file
@@ -112,8 +134,8 @@ Iterate, as if foreach (@{\$data->{'outer'}}) {render(..)} : @outer[
     Top-level value visible: file=$file
     Level1 name overrides top-level: $name
     Level1 outerValue: $outerValue
-    Starting inner list:
-    @inner[Inner $name
+    Starting inner list: @inner[
+    Inner $name
       Level2 name overrides level1: $name
       Top-level values still visible: file=$file
       Level1 value visible: outerValue=$outerValue
@@ -126,9 +148,9 @@ List of lists @lol[
   Elements of array $_
     \$0 = $0
     \$2 = $2 
-    \$n does catenate in an iteration, but that's deprecated: $0Catenated
-    balanced brackets cause no problems: [ x ]
-    And, you can simply get the elements of a list: @_[ $_ ]]
+    \$0..\$9 (and \$_) does catenate within an iteration, but that's deprecated: $0Catenated $_Catenated
+    balanced brackets cause no problems: [ x ]. Nor escaped ones: \] 
+    And, you can simply get the elements of a list: @_[$_ ]]
     (Note the "\$no_recursive_interpolation_of_data" value above)
 end lol
 
